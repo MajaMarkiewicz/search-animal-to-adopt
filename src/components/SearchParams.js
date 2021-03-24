cdd04868498406fe7e5f3bcddd030801f83fdad0
MajaMarkiewicz <markiewicz.maja@gmail.com>
2021-03-24 13:08:11 +0100

    init repository with basic search input

---
+++ src/components/SearchParams.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA")
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          location
+          <input id="location"
+                 value={location}
+                 placeholder="location"
+                 onChange={ e => setLocation(e.target.value)}
+          />
+          <button> Submit </button>
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default SearchParams
