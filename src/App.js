5535db3316a638f84ad91dd6e10628d95894be3d
MajaMarkiewicz <markiewicz.maja@gmail.com>
2021-03-27 19:57:22 +0100

    Implement react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { HashRouter, Route } from 'react-router-dom'
 import SearchParams from './components/SearchParams'
+import Details from "./components/Details";
 
 const App = () => {
   return (
-    <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <HashRouter>
+        <div>
+          <h1 className="logo">Adopt Me!</h1>
+            <Route exact path="/" component={SearchParams}/>
+            <Route path="/details" component={Details} />
+        </div>
+      </HashRouter>
+    </React.StrictMode>
   )
 
 }
